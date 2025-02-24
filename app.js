dc56deafee2c5371213eead287609d32be16c711
santoswf <mateussantosferreira.pp@gmail.com>
2025-02-24 13:38:52 -0300

    at

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active")
-);
+// hamburguer.addEventListener("click", () => nav.classList.toggle("active")
+// );
+
+if (nav && hamburguer) {
+  hamburguer.addEventListener("click", function () {
+    nav.classList.toggle("active"); // Adiciona/remove a classe "active"
+  });
+} else {
+  console.error("Erro: Elementos não encontrados. Verifique a estrutura do HTML.");
+}
+
+const menuLinks = document.querySelectorAll('.menu a[href^="#"]');
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+// function nativeScroll(distanceFromTheTop) {
+//   window.scroll({
+//     top: distanceFromTheTop,
+//     behavior: "smooth",
+//   });
+// }
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+  smoothScrollTo(0, distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 500;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
